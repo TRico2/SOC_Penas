e5a8dd0f227ca29a314e1679ed8a41aa18d8753c
Tomáš Peňás <100348333+TRico2@users.noreply.github.com>
2024-02-15 23:03:43 +0100

    Update coordinator.py

--- AZ_Router_integration/coordinator.py
+++ AZ_Router_integration/coordinator.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the AzRouterT platform."""
+    """Set up the AzRouter platform."""
     coordinator = AZRouterDataUpdateCoordinator(hass)
     await coordinator.async_refresh()
 
@@ -30,7 +30,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
 
 class AZRouterDataUpdateCoordinator(DataUpdateCoordinator):
-    """AzRouterT Data Coordinator."""
+    """AzRouter Data Coordinator."""
 
     def __init__(self, hass: HomeAssistant, config: ConfigEntry):
         """Initialize the data coordinator."""
